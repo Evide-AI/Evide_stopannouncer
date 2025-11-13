f015aefa2fc6e7c3eecae766735d1e1ddf84578d
SANDRA MATHEW <95283546+DUDU0123@users.noreply.github.com>
2025-11-13 16:51:00 +0530

    Clean up: Remove unused video initialization code
    Removed commented-out code for video initialization.

--- lib/features/ads_play_page/presentation/pages/ads_play_page.dart
+++ lib/features/ads_play_page/presentation/pages/ads_play_page.dart
@@ -1,11 +1,15 @@
 import 'dart:developer';
 import 'dart:io';
 import 'package:better_player_plus/better_player_plus.dart';
+import 'package:evide_stop_announcer_app/core/app_imports.dart';
 import 'package:evide_stop_announcer_app/core/common/bus_data_cubit/bus_data_cubit.dart';
+import 'package:evide_stop_announcer_app/core/constants/backend_constants.dart';
+import 'package:evide_stop_announcer_app/core/services/websocket_services.dart';
 import 'package:evide_stop_announcer_app/core/services/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
 
 class AdsPlayPage extends StatefulWidget {
   const AdsPlayPage({super.key});
@@ -18,11 +22,24 @@ class _AdsPlayPageState extends State<AdsPlayPage> {
   BetterPlayerController? _betterPlayerController;
   int currentVideoIndex = 0;
   List<String> _videoList = [];
+  late io.Socket socket;
 
   @override
   void initState() {
     super.initState();
     context.read<BusDataCubit>().getBusData();
+    socket = io.io(
+      BackendConstants.webSocketUrl,
+      io.OptionBuilder()
+          .setTransports(['websocket', 'polling'])
+          .setPath('/socket.io/')
+          .enableReconnection()
+          .setReconnectionDelay(1000)
+          .setReconnectionDelayMax(5000)
+          .setReconnectionAttempts(5)
+          .setTimeout(20000)
+          .build(),
+    );
   }
 
   @override
@@ -30,39 +47,6 @@ class _AdsPlayPageState extends State<AdsPlayPage> {
     _betterPlayerController?.dispose();
     super.dispose();
   }
-
-  // Future<void> initializeVideo({required int index}) async {
-  //   if (_videoList.isEmpty) return;
-
-  //   final videoFile = File(_videoList[index]);
-
-  //   final dataSource = BetterPlayerDataSource(
-  //     BetterPlayerDataSourceType.file,
-  //     videoFile.path,
-  //     notificationConfiguration: const BetterPlayerNotificationConfiguration(
-  //       showNotification: false,
-  //     ),
-  //   );
-
-  //   _betterPlayerController?.dispose(); // Dispose previous controller
-
-  //   _betterPlayerController = BetterPlayerController(
-  //     BetterPlayerConfiguration(
-  //       autoPlay: true,
-  //       looping: false, // Do not loop single video
-  //       handleLifecycle: true,
-  //       allowedScreenSleep: false,
-  //       autoDispose: true,
-  //       eventListener: _onVideoEvent,
-  //       controlsConfiguration: const BetterPlayerControlsConfiguration(
-  //         showControls: false,
-  //       ),
-  //     ),
-  //     betterPlayerDataSource: dataSource,
-  //   );
-  //   _betterPlayerController?.setVolume(0.0);
-  //   setState(() {});
-  // }
 
   Future<void> initializeVideo({required int index}) async {
   if (_videoList.isEmpty) return;
