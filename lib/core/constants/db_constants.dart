942d202da00026a14d673a866712da5242822911
SANDRA MATHEW <95283546+DUDU0123@users.noreply.github.com>
2025-11-13 16:46:29 +0530

    Add busId constant and update stopAudios name

--- lib/core/constants/db_constants.dart
+++ lib/core/constants/db_constants.dart
@@ -3,8 +3,9 @@ class DbConstants {
   static const String busesCollection = 'buses';
 
   // document field names
+  static const String busId = 'bus_id';
   static const String busName = 'bus_name';
   static const String busNumberPlate = 'bus_no_plate';
   static const String adVideos = 'ad_videos';
-  static const String stopAudios = 'stop_audios';
+  static const String stopAudios = 'Stop_audio';
 }
