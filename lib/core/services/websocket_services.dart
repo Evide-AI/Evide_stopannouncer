5166cac8885e237570b021b54ccf1b764d90d6a4
DUDU0123 <sdu200115@gmail.com>
2025-11-16 08:43:05 +0530

    moved socketconnect method to websocket services file and refactored

--- lib/core/services/websocket_services.dart
+++ lib/core/services/websocket_services.dart
@@ -1,76 +1,103 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:better_player_plus/better_player_plus.dart';
+import 'package:evide_stop_announcer_app/core/app_imports.dart';
+import 'package:evide_stop_announcer_app/core/common/bus_data_cubit/bus_data_cubit.dart';
 import 'package:evide_stop_announcer_app/core/common/bus_data_domain/entity/timeline_entity.dart';
 import 'package:evide_stop_announcer_app/core/constants/app_global_keys.dart';
 import 'package:evide_stop_announcer_app/features/ads_play_page/presentation/dialogs/current_stop_data_showing_dialog.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
+
 class WebSocketServices {
-  static int? _lastShownStopSequence;
+  static int? lastShownStopSequence;
 
-  static void joinTrip(io.Socket socket, int tripId) {
-  socket.emitWithAck('join-trip', {'tripId': tripId}, ack: (data) {
-    log('🟢 Trip joined ACK: $data');
-  });
-}
-
-  /// ✅ Connect to backend socket
-  static void connectSocket({
-    required TimeLineEntity activeTripTimelineData,
-    required io.Socket socket,
-    required Map<String, dynamic> stopAudios,
-    required AudioPlayer audioPlayer,
-  }) {
-    socket.connect();
+  static void connectAndListenToSocket({required io.Socket socket, required BuildContext context, required BetterPlayerController? betterPlayerController, required AudioPlayer audioPlayer}) {
+    // on connection established join the trip room
     socket.onConnect((_) {
-      log('🟢 Connected: ${socket.id}');
-      final tripId = activeTripTimelineData.tripDetails?.id;
+      final busData = context.read<BusDataCubit>().state.busData;
+      log('🟢 Socket Connected: ${socket.id}');
+      final tripId = busData.activeTripTimelineModel?.tripDetails?.id;
       if (tripId != null) {
-        joinTrip(socket, tripId);
+        log('🚍 Joining trip: $tripId');
+        socket.emit('join-trip', {'tripId': tripId});
       }
     });
 
-    socket.onReconnect((attempt) {
-      log('🔄 Reconnected (attempt $attempt)');
-      final tripId = activeTripTimelineData.tripDetails?.id;
-      if (tripId != null) {
-        joinTrip(socket, tripId);
-      }
-    });
-
-
-    socket.onDisconnect((reason) {
-      log('🔴 Disconnected: $reason');
-    });
-    socket.onError((data) => log('🚨 Socket Error: $data'));
-    socket.onConnectError((data) => log('❌ Connect Error: $data'));
-
+    // if joined trip
     socket.on('joined-trip', (data) => log('✅ Joined trip: $data'));
 
-    /// ✅ Location update handler
+    // if trip ended fetch new trip data of the bus
+    socket.on("trip-ended", (data) {
+      log('🏁 Trip ended: $data');
+      // Leave previous trip room
+      if (data != null && data['tripId'] != null) {
+        socket.emit("leave-trip", {"tripId": data['tripId']});
+      }
+      // Fetch new bus data
+      context.read<BusDataCubit>().getBusData();
+    });
+
+    // if disconnected
+    socket.onDisconnect((reason) => log('🔴 Disconnected: $reason'));
+    socket.onError((data) => log('🚨 Socket Error: $data'));
+
+    // on error (like trip not found or not active)
+    socket.on('error', (error) {
+      log('❌ Server Error: $error');
+    });
+
+    // on gps location update
     socket.on('location-update', (data) {
       final jsonData = data is String ? jsonDecode(data) : data;
-      final currentStopSequenceNumber = jsonData['current_stop_sequence_number'];
+      final currentStopSequenceNumber =
+          jsonData['current_stop_sequence_number'];
+      log("📍 location data: $jsonData");
 
       try {
-        for (StopEntity stop in activeTripTimelineData.stopList ?? []) {
+        for (StopEntity stop
+            in context
+                    .read<BusDataCubit>()
+                    .state
+                    .busData
+                    .activeTripTimelineModel
+                    ?.stopList ??
+                []) {
           if (stop.sequenceOrder == currentStopSequenceNumber) {
             log('🏁 Arrived: ${stop.stopName}');
 
-            if (_lastShownStopSequence != currentStopSequenceNumber) {
+            if (lastShownStopSequence != currentStopSequenceNumber) {
               // PLAY AUDIO
-              final audioUrl = stopAudios[stop.stopId.toString()];
+              final audioUrl = context
+                  .read<BusDataCubit>()
+                  .state
+                  .busData
+                  .stopAudios?[stop.stopId.toString()];
               if (audioUrl != null) {
+                /// 🔇 Mute video before playing stop audio
+                betterPlayerController?.videoPlayerController?.setVolume(0.0);
+
                 audioPlayer.play(UrlSource(audioUrl));
+
+                /// 🟢 When stop audio completes – restore video volume
+                Future.delayed(Duration(seconds: 1), () {
+                  audioPlayer.onPlayerComplete.listen((event) {
+                    betterPlayerController?.videoPlayerController?.setVolume(
+                      0.01,
+                    );
+                  });
+                });
               }
 
               // SHOW DIALOG
               currentStopDataShowingDialog(
-                context: AppGlobalKeys.navigatorKey.currentState!.overlay!.context,
+                context:
+                    AppGlobalKeys.navigatorKey.currentState!.overlay!.context,
                 stopName: stop.stopName ?? 'Unknown Stop',
               );
             }
-            _lastShownStopSequence = currentStopSequenceNumber;
+            lastShownStopSequence = currentStopSequenceNumber;
           }
         }
       } catch (e) {
@@ -78,12 +105,12 @@ class WebSocketServices {
       }
     });
 
-  }
+    // on connection error occured
+    socket.onConnectError((data) {
+      log('❌ Socket Connect Error: $data');
+    });
 
-
-  void leaveTrip({required int tripId, required io.Socket socket}) {
-    if (socket.connected) {
-      socket.emit('leave-trip', {'tripId': tripId});
-    }
+    // connect the socket
+    socket.connect(); // only once
   }
 }
--- lib/features/ads_play_page/presentation/pages/ads_play_page.dart
+++ lib/features/ads_play_page/presentation/pages/ads_play_page.dart
@@ -1,14 +1,11 @@
-import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:better_player_plus/better_player_plus.dart';
 import 'package:evide_stop_announcer_app/core/app_imports.dart';
 import 'package:evide_stop_announcer_app/core/common/bus_data_cubit/bus_data_cubit.dart';
-import 'package:evide_stop_announcer_app/core/common/bus_data_domain/entity/timeline_entity.dart';
-import 'package:evide_stop_announcer_app/core/constants/app_global_keys.dart';
 import 'package:evide_stop_announcer_app/core/constants/backend_constants.dart';
-import 'package:evide_stop_announcer_app/features/ads_play_page/presentation/dialogs/current_stop_data_showing_dialog.dart';
+import 'package:evide_stop_announcer_app/core/services/websocket_services.dart';
 import 'package:evide_stop_announcer_app/features/ads_play_page/presentation/widgets/ads_play_page_common_loading_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -36,7 +33,9 @@ class _AdsPlayPageState extends State<AdsPlayPage> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<BusDataCubit>().getBusData();
       Future.delayed(const Duration(seconds: 2), () {
-        connectAndListenToSocket();
+        if (mounted) {
+          WebSocketServices.connectAndListenToSocket(socket: socket, context: context, betterPlayerController: _betterPlayerController, audioPlayer: audioPlayer);
+        }
       });
     });
   }
@@ -56,89 +55,89 @@ class _AdsPlayPageState extends State<AdsPlayPage> {
     );
   }
 
-  connectAndListenToSocket() {
-    // on connection established join the trip room
-    socket.onConnect((_) {
-      final busData = context.read<BusDataCubit>().state.busData;
-      log('🟢 Socket Connected: ${socket.id}');
-      final tripId = busData.activeTripTimelineModel?.tripDetails?.id;
-      if (tripId != null) {
-        log('🚍 Joining trip: $tripId');
-        socket.emit('join-trip', {'tripId': tripId});
-      }
-    });
+  // void connectAndListenToSocket() {
+  //   // on connection established join the trip room
+  //   socket.onConnect((_) {
+  //     final busData = context.read<BusDataCubit>().state.busData;
+  //     log('🟢 Socket Connected: ${socket.id}');
+  //     final tripId = busData.activeTripTimelineModel?.tripDetails?.id;
+  //     if (tripId != null) {
+  //       log('🚍 Joining trip: $tripId');
+  //       socket.emit('join-trip', {'tripId': tripId});
+  //     }
+  //   });
 
-    // if joined trip
-    socket.on('joined-trip', (data) => log('✅ Joined trip: $data'));
+  //   // if joined trip
+  //   socket.on('joined-trip', (data) => log('✅ Joined trip: $data'));
 
-    // if trip ended fetch new trip data of the bus
-   socket.on("trip-ended", (data) {
-    log('🏁 Trip ended: $data');
-    // Leave previous trip room
-    if (data != null && data['tripId'] != null) {
-      socket.emit("leave-trip", { "tripId": data['tripId'] });
-    }
-    // Fetch new bus data
-    context.read<BusDataCubit>().getBusData();
-  });
+  //   // if trip ended fetch new trip data of the bus
+  //  socket.on("trip-ended", (data) {
+  //   log('🏁 Trip ended: $data');
+  //   // Leave previous trip room
+  //   if (data != null && data['tripId'] != null) {
+  //     socket.emit("leave-trip", { "tripId": data['tripId'] });
+  //   }
+  //   // Fetch new bus data
+  //   context.read<BusDataCubit>().getBusData();
+  // });
 
-    // if disconnected
-    socket.onDisconnect((reason) => log('🔴 Disconnected: $reason'));
-      socket.onError((data) => log('🚨 Socket Error: $data'));
+  //   // if disconnected
+  //   socket.onDisconnect((reason) => log('🔴 Disconnected: $reason'));
+  //     socket.onError((data) => log('🚨 Socket Error: $data'));
 
-    // on error (like trip not found or not active)
-    socket.on('error', (error) {log('❌ Server Error: $error');});
+  //   // on error (like trip not found or not active)
+  //   socket.on('error', (error) {log('❌ Server Error: $error');});
 
-    // on gps location update
-    socket.on('location-update', (data) {
-      final jsonData = data is String ? jsonDecode(data) : data;
-      final currentStopSequenceNumber = jsonData['current_stop_sequence_number'];
-      log("📍 location data: $jsonData");
+  //   // on gps location update
+  //   socket.on('location-update', (data) {
+  //     final jsonData = data is String ? jsonDecode(data) : data;
+  //     final currentStopSequenceNumber = jsonData['current_stop_sequence_number'];
+  //     log("📍 location data: $jsonData");
 
-      try {
-        for (StopEntity stop in context.read<BusDataCubit>().state.busData.activeTripTimelineModel?.stopList ?? []) {
-          if (stop.sequenceOrder == currentStopSequenceNumber) {
-            log('🏁 Arrived: ${stop.stopName}');
+  //     try {
+  //       for (StopEntity stop in context.read<BusDataCubit>().state.busData.activeTripTimelineModel?.stopList ?? []) {
+  //         if (stop.sequenceOrder == currentStopSequenceNumber) {
+  //           log('🏁 Arrived: ${stop.stopName}');
 
-            if (lastShownStopSequence != currentStopSequenceNumber) {
-              // PLAY AUDIO
-              final audioUrl = context.read<BusDataCubit>().state.busData.stopAudios?[stop.stopId.toString()];
-              if (audioUrl != null) {
-                /// 🔇 Mute video before playing stop audio
-                _betterPlayerController?.videoPlayerController?.setVolume(0.0);
+  //           if (lastShownStopSequence != currentStopSequenceNumber) {
+  //             // PLAY AUDIO
+  //             final audioUrl = context.read<BusDataCubit>().state.busData.stopAudios?[stop.stopId.toString()];
+  //             if (audioUrl != null) {
+  //               /// 🔇 Mute video before playing stop audio
+  //               _betterPlayerController?.videoPlayerController?.setVolume(0.0);
 
-                audioPlayer.play(UrlSource(audioUrl));
+  //               audioPlayer.play(UrlSource(audioUrl));
 
-                /// 🟢 When stop audio completes – restore video volume
-                Future.delayed(Duration(seconds: 1), () {
-                  audioPlayer.onPlayerComplete.listen((event) {
-                    _betterPlayerController?.videoPlayerController?.setVolume(0.01);
-                  });
-                },);
-              }
+  //               /// 🟢 When stop audio completes – restore video volume
+  //               Future.delayed(Duration(seconds: 1), () {
+  //                 audioPlayer.onPlayerComplete.listen((event) {
+  //                   _betterPlayerController?.videoPlayerController?.setVolume(0.01);
+  //                 });
+  //               },);
+  //             }
 
-              // SHOW DIALOG
-              currentStopDataShowingDialog(
-                context: AppGlobalKeys.navigatorKey.currentState!.overlay!.context,
-                stopName: stop.stopName ?? 'Unknown Stop',
-              );
-            }
-            lastShownStopSequence = currentStopSequenceNumber;
-          }
-        }
-      } catch (e) {
-        log('❌ Error parsing location-update: $e');
-      }
-    });
+  //             // SHOW DIALOG
+  //             currentStopDataShowingDialog(
+  //               context: AppGlobalKeys.navigatorKey.currentState!.overlay!.context,
+  //               stopName: stop.stopName ?? 'Unknown Stop',
+  //             );
+  //           }
+  //           lastShownStopSequence = currentStopSequenceNumber;
+  //         }
+  //       }
+  //     } catch (e) {
+  //       log('❌ Error parsing location-update: $e');
+  //     }
+  //   });
 
-    // on connection error occured
-    socket.onConnectError((data) {
-      log('❌ Socket Connect Error: $data');
-    });
+  //   // on connection error occured
+  //   socket.onConnectError((data) {
+  //     log('❌ Socket Connect Error: $data');
+  //   });
 
-    // connect the socket
-    socket.connect(); // only once
-  }
+  //   // connect the socket
+  //   socket.connect(); // only once
+  // }
 
 
   @override
