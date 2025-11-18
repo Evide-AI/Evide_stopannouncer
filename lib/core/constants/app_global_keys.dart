4eb749aba72909b30d528658473e469a9309db04
SANDRA MATHEW <95283546+DUDU0123@users.noreply.github.com>
2025-11-18 14:28:07 +0530

    Add appAdminAccessSecretKey to AppGlobalKeys

--- lib/core/constants/app_global_keys.dart
+++ lib/core/constants/app_global_keys.dart
@@ -2,4 +2,5 @@ import 'package:evide_stop_announcer_app/core/app_imports.dart';
 
 class AppGlobalKeys {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static String appAdminAccessSecretKey = "112266";
 }
