dbe6e9f65300ee41e15f5e9859815f733735e3b0
SANDRA MATHEW <95283546+DUDU0123@users.noreply.github.com>
2025-11-20 17:19:11 +0530

    Implement StreamBusVideosUsecase for video streaming

---
+++ lib/core/common/bus_data_domain/usecases/stream_bus_videos_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:evide_stop_announcer_app/core/common/bus_data_domain/bus_data_repo/bus_repo.dart';
+import 'package:evide_stop_announcer_app/core/failure/failure.dart';
+import 'package:evide_stop_announcer_app/core/usecase/stream_usecase.dart';
+import 'package:fpdart/fpdart.dart';
+
+class StreamBusVideosUsecase implements StreamUsecase<List<String>, String>{
+  final BusDataRepo busDataRepo;
+
+  StreamBusVideosUsecase({required this.busDataRepo}); 
+  @override
+  Stream<Either<Failure, List<String>>> call({required String params}) {
+    return busDataRepo.streamBusVideos(busPairingCode: params);
+  }
+}
