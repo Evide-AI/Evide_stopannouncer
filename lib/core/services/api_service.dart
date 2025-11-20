78c40756657973c3b1f18e058401d4b268e4be3c
SANDRA MATHEW <95283546+DUDU0123@users.noreply.github.com>
2025-11-20 17:09:54 +0530

    Add ApiService class for API interactions
    Implemented ApiService class with GET and POST methods, including connection checking before requests.

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:evide_stop_announcer_app/core/network_connection/connection_checker.dart';
+import 'package:evide_stop_announcer_app/core/services/api_services/api_core.dart';
+
+class ApiService {
+
+  ApiService() : apiCore = ApiCore(interceptors: [apiServiceInterceptor]);
+
+  final ApiCore apiCore;
+
+  // interceptor for ApiService
+  static InterceptorsWrapper get apiServiceInterceptor => InterceptorsWrapper(
+    onRequest: ((options, handler) async {
+      // isNeedToWait added to check internet connection before making the request
+      final bool isNeedToWait = options.extra['isNeedToWait'] ?? true;
+      if (isNeedToWait) {
+        final completer = Completer<void>();
+        ConnectionChecker().connectionStream.listen((isConnected) {
+          if (isConnected) {
+            if (!completer.isCompleted) {
+              completer.complete(); // Resolve completer when connection is restored
+            }
+          }
+        });
+        final isConnected = await ConnectionChecker.checkConnectivity();
+        // Wait for connection to be restored if there's no internet
+        if (!isConnected) {
+          await completer.future; // Wait until the completer is completed
+        }
+        handler.next(options);
+      } else {
+        handler.next(options);
+      }
+    }),
+    onResponse: (e, handler) {
+      handler.next(e);
+    },
+    onError: (e, handler) async {
+      handler.next(e);
+    },
+  );
+  // get method
+  Future<Response?> get({
+    required String url,
+    CancelToken? cancelToken,
+    int timeout = 60,
+    bool? isNeedToWait,
+    bool updateToken = false,
+  }) {
+    return apiCore.get(
+      url: url,
+      cancelToken: cancelToken,
+      timeout: timeout,
+      isNeedToWait: isNeedToWait,
+    );
+  }
+
+  // post method
+  Future<Response?> post({
+    required String url,
+    required Map<String, dynamic> data,
+    CancelToken? cancelToken,
+    Map<String, String?>? additionalHeaders,
+    bool? isNeedToWait,
+  }) {
+    return apiCore.post(
+        url: url,
+        data: jsonEncode(data),
+        cancelToken: cancelToken,
+        additionalHeaders: additionalHeaders,
+        isNeedToWait: isNeedToWait,
+      );
+  }
+}
