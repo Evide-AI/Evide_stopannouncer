1bc2fff006408e766ddf3dd40095f8899a38cb0d
DUDU0123 <sdu200115@gmail.com>
2025-11-30 11:36:18 +0530

    kiosk mode code uncommented

--- lib/core/services/kiosk_mode_service.dart
+++ lib/core/services/kiosk_mode_service.dart
@@ -5,13 +5,17 @@ import 'package:kiosk_mode/kiosk_mode.dart';
 class KioskModeService {
   // method for enabling kiosk mode
   static Future<void> enableKioskMode() async {
-    // try {
-    //   await startKioskMode();
-    //   await LauncherPermission.requestSetDefaultLauncher();
-    //   debugPrint("✅ Kiosk mode enabled successfully");
-    // } catch (e) {
-    //   debugPrint("❌ Error enabling kiosk mode: $e");
-    // }
+    try {
+      await startKioskMode();
+      try {
+        await LauncherPermission.requestSetDefaultLauncher();
+      } catch (e) {
+        debugPrint("❌ Error requestSetDefaultLauncher: $e");
+      }
+      debugPrint("✅ Kiosk mode enabled successfully");
+    } catch (e) {
+      debugPrint("❌ Error enabling kiosk mode: $e");
+    }
   }
   // method for disablinig kiosk mode
   static Future<void> disableKioskMode() async {
